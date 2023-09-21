fc4150bc5bd303bfc091c429c9b394a6cd27aa51
깃sdsr <dlxodnr1212@gmail.com>
2023-09-21 16:04:54 +0900

    "유틸리티 객체와 날짜"

--- src/main/java/hello/thymleaf/basic/BasicController.java
+++ src/main/java/hello/thymleaf/basic/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -57,6 +58,12 @@ public class BasicController {
         model.addAttribute("servletContext", request.getServletContext());
 
         return "basic/basic-objects";
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
     }
 
     @Component("helloBean")
